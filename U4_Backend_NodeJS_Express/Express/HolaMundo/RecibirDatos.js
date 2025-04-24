44756d3cd90115809a45ba1b5b1f0be88a0d25f5
David Rivera A <davidriveraarbelaez@gmail.com>
2025-04-23 20:35:09 -0500

    Recibir datos

--- U4_Backend_NodeJS_Express/Express/HolaMundo/RecibirDatos.js
+++ U4_Backend_NodeJS_Express/Express/HolaMundo/RecibirDatos.js
@@ -5,7 +5,7 @@ const port = 6000; // Define el puerto en el que se ejecutará el servidor (6000
 
 app.use(bodyParser.json()); // Configura body-parser para analizar el cuerpo de las solicitudes en formato JSON
 
-app.post('usuarios', (req, res) => {
+app.post('/usuarios', (req, res) => {
     const nuevoUsuario = req.body; // Obtiene el nuevo usuario del cuerpo de la solicitud
     console.log('Nuevo usuario recibido:', nuevoUsuario); // Imprime el nuevo usuario en la consola
     res.send('Usuario creado exitosamente'); // Envía una respuesta de éxito al cliente
